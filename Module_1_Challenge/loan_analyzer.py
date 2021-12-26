df09b4eee5cef0fd5a7b2c936d3575101701fdfc
plefebvre1 <95890906+plefebvre1@users.noreply.github.com>
2021-12-26 14:35:52 -0500

    M1C1 Version 1.2 - Part 2 Complete
    Analyze Loan Data - calculate present value, or fair price, of what this loan would be worth

--- Module_1_Challenge/loan_analyzer.py
+++ Module_1_Challenge/loan_analyzer.py
@@ -68,21 +68,29 @@ loan = {
 
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
-# YOUR CODE HERE!
-
+future_value = loan.get("future_value")
+remaining_months = loan.get("remaining_months")
+print(f"The future value of the loan is ${future_value}.")
+print(f"There are {remaining_months} months remaining on the loan.")
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
 # Use a minimum required return of 20% as the discount rate.
 #   You'll want to use the **monthly** version of the present value formula.
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 
-# YOUR CODE HERE!
+discount_rate = 0.20
+present_value = future_value / (1 + discount_rate / 12) ** remaining_months
+print(f"The present value of the loan is ${present_value}.")
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-# YOUR CODE HERE!
+loan_cost = loan.get("loan_price")
+if present_value >= loan_cost:
+    print("The loan is worth at least the cost to buy it.")
+else:
+    print("The loan is too expensive and not worth the price.")
 
 
 """Part 3: Perform Financial Calculations.
